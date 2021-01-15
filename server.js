ea692f61c53e589ace93f19fe9ff73ada035daef
pellmius <imperialball123@gmail.com>
2021-01-14 23:43:27 -0300

    Added Client-Side

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 // Requirements
 const express = require('express');
 const io = require('socket.io');
-//
+const dotenv = require('./config_env')
 
+// Environment Variables and App Configurations
+
+const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.listen()
+
+//Routes
+
+
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`App running at localhost:${PORT}`)
+})
